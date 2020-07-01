29f6cbe00a46598d71d540e7ecde39255139988f
stillraider <stilryder74@mail.ru>
2020-07-01 16:09:37 +0500

    100% summary folder

--- #src/scripts/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".slider").slick({
-        // adaptiveHeight: true,
-        slidesToShow: 1,
-        speed: 600,
-        autoplay: true,
-        autoplaySpeed: 5000,
-    });
-});
